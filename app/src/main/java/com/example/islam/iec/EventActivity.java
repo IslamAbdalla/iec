5eeb3eeca364e93224175f5083d1361d021482d5
Islam Abdalla <islam.a.elamin@gmail.com>
2016-09-25 10:29:35 +0300

    Created basic event activity

--- app/src/main/java/com/example/islam/iec/EventActivity.java
+++ app/src/main/java/com/example/islam/iec/EventActivity.java
@@ -1,17 +1,21 @@
 package com.example.islam.iec;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by islam on 9/15/16.
  */
-public class EventActivity extends AppCompatActivity {
+public class EventActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 }
