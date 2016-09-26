0c62372278ceb4b3e50680d57bda783971a643b1
Islam Abdalla <islam.a.elamin@gmail.com>
2016-09-26 17:13:52 +0300

    Event Activity Header Done

--- app/src/main/java/com/example/islam/iec/EventActivity.java
+++ app/src/main/java/com/example/islam/iec/EventActivity.java
@@ -1,21 +1,55 @@
 package com.example.islam.iec;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by islam on 9/15/16.
  */
-public class EventActivity extends Activity {
+public class EventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.anim_toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.ic_white_back);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        setTitle(" ");
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if(scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if(scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Startup Weekend Khartoum");
+                    isShow = true;
+                } else if(isShow) {
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+
+            }
+        });
+
 
     }
 }
