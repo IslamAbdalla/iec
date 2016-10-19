b1ded92d4246180166b1a3f96dda5a50d9321374
Islam Abdalla <islam.a.elamin@gmail.com>
2016-10-19 18:27:07 +0300

    Removed fab and added update button

--- app/src/main/java/com/example/islam/iec/ProfileActivity.java
+++ app/src/main/java/com/example/islam/iec/ProfileActivity.java
@@ -1,14 +1,19 @@
 package com.example.islam.iec;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.print.PrintHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
     private PrefManager prefManager;
     private User user;
+    private Boolean updateMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,23 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         prefManager = new PrefManager(this);
 
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.profile_fab);
+//        if (fab != null) {
+//            fab.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Snackbar.make(view, "We will proceed with updating your info", Snackbar.LENGTH_LONG)
+//                            .setAction("Action", null).show();
+//                }
+//            });
+//        }
+        updateMode = false;
+
         if (prefManager.isLoggedIn()) {
             user = prefManager.getUser();
             TextView username = (TextView) findViewById(R.id.profile_username);
             TextView name = (TextView) findViewById(R.id.profile_name);
+            TextView password = (TextView) findViewById(R.id.profile_password);
             TextView phone = (TextView) findViewById(R.id.profile_phone);
             TextView email = (TextView) findViewById(R.id.profile_email);
             TextView address = (TextView) findViewById(R.id.profile_address);
@@ -32,6 +50,9 @@ public class ProfileActivity extends AppCompatActivity {
             }
             if (name != null) {
                 name.setText(user.getName());
+            }
+            if (password != null) {
+                password.setText(user.getPassword());
             }
             if (phone != null) {
                 phone.setText(user.getPhone());
@@ -51,5 +72,8 @@ public class ProfileActivity extends AppCompatActivity {
             finish();
         }
 
+    }
+
+    public void updateUserData(View view) {
     }
 }
